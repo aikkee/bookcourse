39818af5321ca5b524d091dc96b4c857389d5e9a
aikkee <aikkee@gmail.com>
2020-11-08 17:04:59 +0800

    added new courses

--- wsgi.py
+++ wsgi.py
@@ -66,7 +66,7 @@ def init():
         mod = request.args.get('mod')
         course = request.args.get('course')
         hardcopy = request.args.get('hardcopy')
-        course_id, course_desc = course.split(":")
+        course_id, course_desc = course[:course.index(":")], course[course.index(":")+1:]
         isnew = request.args.get('isnew')
         rtype = get_rtype(ref)
         # print(f'ref:{ref}, course:{course}, isnew:{isnew}, rtype:{rtype}')
@@ -82,17 +82,22 @@ def init():
     except:
         return redirect(url_for('index'))
 
-    # TODO: Improve the query later by combining the 2 statements
     reference = Reference.query.filter(Reference.booking_ref==ref+"**"+course_id).first()
     booked_resource = Resource.query.filter(Resource.id==reference.resource_id).first()
     # Need to also handle cases where user use the same link to amend booking
     # When initiated from user through the sms link, isnew will be 'yes'
     # When initiated from change_booking page, isnew will be 'no'
     if (isnew=='yes' and booked_resource!=None):
-        m = {'location':booked_resource.location, \
-              'date_time':booked_resource.description}
-        b = [{'ref':ref, 'course':course, 'rtype':rtype, 'hardcopy':reference.hardcopy, 'isnew':'no', 'label':'Yes'}]
-        return render_template('change_booking.html', message=m, buttons=b)
+        # TODO: Handles wait list
+        if ("31 DEC 2021" in booked_resource.description):
+            m = {'location':booked_resource.location}
+            b = [{'label':'OK'}]
+            return render_template('on_waiting_list.html', message=m, buttons=b)
+        else:
+            m = {'location':booked_resource.location, \
+                  'date_time':booked_resource.description}
+            b = [{'ref':ref, 'course':course, 'rtype':rtype, 'hardcopy':reference.hardcopy, 'isnew':'no', 'label':'Yes'}]
+            return render_template('change_booking.html', message=m, buttons=b)
 
     # obtain the resource slots from database
     # available_resource = Resource.query.filter(Resource.available>0)
@@ -104,10 +109,15 @@ def init():
     resources = Resource.query.filter(and_(Resource.location==course, Resource.available>0, Resource.rtype==rtype)).all()
     
 
-    #locations = list(set([(r.location) for r in resources if show_for_booking(r, str(reference.expire_on), rtype)]))
-    #bookingForm.location.choices = [('', 'Choose Course')] + [
-    #    (l, l) for l in locations]
-    course_run = [(r.id, r.description) for r in resources if show_for_booking(r, str(reference.expire_on), rtype)]
+    # course_run = [(r.id, r.description) for r in resources if show_for_booking(r, str(reference.expire_on), rtype)]
+    # TODO: Handles wait list
+    course_run = []
+    for r in resources:
+        if (show_for_booking(r, str(reference.expire_on), rtype)):
+            if ("31 DEC 2021" in r.description):
+                course_run.append((r.id, "Fully booked. Notify me if new slots are available"))
+            else:
+                course_run.append((r.id, r.description))
     bookingForm.date_time.choices = [('', 'Choose Course Date/Time')] + course_run
     
     bookingForm.booking_ref.data = ref + "**" + course_id
@@ -179,7 +189,12 @@ def save():
         b = [{'label':'OK'}]
         # l = {'url': get_gcal_url(resource.location, resource.description)}
         # return render_template('acknowledge.html', message=m, gcal_link=l)
-        return render_template('acknowledge.html', message=m, buttons=b)
+
+        # TODO: Handles wait list
+        if ("31 DEC 2021" in resource.description):
+            return render_template('on_waiting_list.html', message=m, buttons=b)
+        else:
+            return render_template('acknowledge.html', message=m, buttons=b)
 
 """
 def get_gcal_url(location, dt_str):
@@ -244,11 +259,123 @@ def get_course_description(code):
         "CC-CC102":"Job Mgmt, Data Struct. & Analytics in HRP",
         "CC-CC103":"Approvals & Authorisations in HRP",
         "CC-CC104":"Substantive Appt., Payroll Ov. & Basic Pay in HRP",
+        "ATT-GEN101A":"External & Internal Recruitment",
+        "ATT-GEN101B":"External & Internal Recruitment (Stat Boards not in HRP)",
         "ATT-GEN102":"Re-Employment",
+        "ATT-GEN103":"In-Service Appointment",
         "ATT-GEN104":"Part-Time Employment",
+        "ATT-GEN109":"Personnel & Data Entity Management",
         "ATT-MHA106M":"Enlistment Process for HRMG",
-        "DVP-MHA102":"Training Admin-Nomination & Course",
-        "PAY-GEN101":"Pre-Payroll - Allowance & Deduction Processing and Payroll Retro Adjuster"
+        "ATT-MHA106T":"Enlistment Process for Training School",
+        "ATT-MHA106S":"Enlistment Process for NS Shared Services",
+        "ATT-MOE101":"Recruitment of Untrained Teachers",
+        "ATT-MOE103":"Appointment of Untrained Teachers to Trained Grade",
+        "ATT-MOE105":"Anniversary Promotion & Contract Upgrading",
+        "DEP-GEN101":"Organisation Data Management",
+        "DEP-GEN102":"Manpower Planning",
+        "DEP-GEN103":"Succession Planning",
+        "DEP-GEN104":"Deployment",
+        "DEP-GEN105":"Secondment",
+        "DEP-MHA101":"Organisation Data Management for MHA",
+        "DEP-MHA104":"Deployment for MHA",
+        "DEP-MHA106":"Vocation for NSF/Nsmen and Extension of Service",
+        "DEP-MOE102H":"School Staffing for HR Partners",
+        "DEP-MOE104HA":"HQ Staffing & MOE Posting Exercise",
+        "DEP-MOE104HB":"Centralised Posting with Posting Matching for HR",
+        "DEP-MOE104HC":"Talent Group Posting",
+        "DEP-MOE106H":"Maintain Teaching Information for HR",
+        "DVP-GEN101":"Job Architecture & Competencies",
+        "DVP-GEN102":"Training Administration (Nomination & Course)",
+        "DVP-GEN103":"Sponsorship (In-Service)",
+        "DVP-MHA102":"Training Administration (Nomination & Course) for MHA",
+        "PFM-GEN101":"Performance Management",
+        "PFM-PSD102":"Policy Related Ranking & Promotion",
+        "PFM-GEN102":"Ranking & Promotion",
+        "PFM-GEN103A":"Ranking & Promotion Endorsement",
+        "PFM-GEN103B":"Ranking & Promotion Management of Appeals",
+        "PFM-GEN104":"Discipline",
+        "PFM-GEN105":"Credit Screening",
+        "PFM-GEN106":"Agency Specific Credit Screening for MOE & MHA",
+        "PFM-GEN107":"Blacklist Administration",
+        "PFM-MHA101":"Performance Management for NSF & NSmen",
+        "PFM-MHA103F":"Ranking & Promotion for NSF",
+        "PFM-MHA103M":"Ranking & Promotion for NSmen",
+        "PFM-MHA104":"Discipline for NSF and NSmen",
+        "PFM-MHA107":"Blacklist Administration for MHA (NS related)",
+        "RWD-PSD101":"Policy Related Compensation & Allowances",
+        "RWD-GEN102":"Compensation & Allowances for MRU & MRA (Generic PRP)",
+        "RWD-GEN103":"Compensation & Allowances for MRU & MRA (Ministry Specific PRP)",
+        "RWD-GEN104":"Compensation & Allowances for PRP Administrator",
+        "RWD-GEN105":"Awards Administration for LSA & SAA",
+        "RWD-GEN106":"NDA Awards Administration",
+        "RWD-MHA107":"Gazetted, Team Incentive and MHA Specific Awards",
+        "RWD-MHA108":"NS Home Awards and NS Excellence Awards",
+        "RWD-MHA109":"Financial Assistance for NSF",
+        "RWD-MHA110":"Tax Relief for Nsmen",
+        "EXT-GEN101":"Resignation (Voluntary, SRS, SGS)",
+        "EXT-GEN102":"Termination, Retirement in Public Interest, SES and VOC",
+        "EXT-GEN103":"Death and Contract Expiry",
+        "EXT-GEN104":"Retirement, Pre-Retirement and Death Gratuity",
+        "EXT-MHA105":"Exit Management for MHA (Discharge and Disruption)",
+        "EXT-MHA106":"Exit Management for MHA (ORD, ROD and VES)",
+        "EXT-MHA107":"Exit Management for MHA (Career Transition)",
+        "ESS-GEN101":"Medical Screening",
+        "ESS-GEN102":"Medical Board",
+        "ESS-MHA102":"Medical Board for MHA",
+        "ESS-GEN103":"Medical Entitlement",
+        "ESS-GEN104":"Service Injury Compensation for Civilian",
+        "ESS-MHA104":"Service Injury Compensation for Uniformed",
+        "ESS-GEN105":"Security Screening",
+        "ESS-GEN106":"Declarations",
+        "ESS-MHA106":"Declarations for MHA",
+        "ESS-GEN107":"Time Data",
+        "ESS-GEN108":"Leave Administration",
+        "PEN-GEN101":"Pension Administration",
+        "PEN-GEN102":"Pension Payment Processing",
+        "PAY-GEN101":"Pre-Payroll - Allowance & Deduction Processing and Payroll Retro Adjuster",
+        "PAY-MOE101":"Pre-Payroll - RA, SA and SEP for MOE",
+        "PAY-GEN102":"Pre-Payroll - Annual Increment and Non-Performance Related Bonus",
+        "PAY-GEN103":"Pre-Payroll - Bank Account Verification Interface (BAV)",
+        "PAY-GEN104":"Pre-Payroll - Salary Revision",
+        "PAY-GEN105":"Pre-Payroll - Payroll Control Center",
+        "PAY-GEN106":"Payroll Processing & BAV Bypass",
+        "PAY-GEN107":"Post Payroll Processing (Financial Activities, Bank Transfer & Recovery Administration)",
+        "PAY-GEN108":"Payroll Reports",
+        "PAY-GEN109":"Statutory Reporting",
+        "PYNS-MHA101":"MHA - NSF Payroll Processing",
+        "PYNS-MHA102A":"MHA - NSMEN Make-Up Pay Processing for Employed",
+        "PYNS-MHA102B":"MHA - NSMEN Make-Up Pay Approval Process for Employed",
+        "PYNS-MHA103A":"MHA - NSMEN Make-Up Pay Processing for Self-Employed",
+        "PYNS-MHA103B":"MHA - NSMEN Make-Up Pay Approval Process for Self-Employed",
+        "PYNS-MHA104":"MHA - NSMEN Make-Up Pay Payroll Processing",
+        "CLA-GEN101":"Claims Processing",
+        "CLA-MHA101":"MHA Specific Claims Approvals & Backend Claims Processes",
+        "MED-GEN101":"Medical Billing",
+        "GRC-GEN101":"HRP Authorisation & Design Overview",
+        "GRC-GEN102":"GRC Administrator Training",
+        "GRC-GEN103":"IA and Compliance Team",
+        "CRM-MHA101":"Overview of CRM and Case Handling Flow",
+        "CRM-MHA102":"CRM Management for Customer Helpdesk",
+        "CRM-MHA103":"Handling Cases for Shared Service Center",
+        "CRM-MHA104":"CRM Dashboards, Approvals & SLA Lapses",
+        "CRM-MHA105":"CRM Administration",
+        "CC101":"Functionalities, Company & Organisation Structure in HRP",
+        "CC102":"Job Management, Data Structure & Analytics",
+        "CC103":"Approvals & Authorisations on HRP",
+        "CC104":"Substantive Appointment, Payroll Overview & Basic Pay on HRP",
+        "CC105":"General Functions",
+        "DVP-LD101":"LD Performance and Potential Ranking & Promotion",
+        "DVP-LD102A":"LD Board Directorship Appointment & Management",
+        "DVP-LD102B":"LD Board Directorship Fees",
+        "DVP-LD103":"LD Engagement",
+        "DVP-LD104":"LD Training Milestone Programme",
+        "DVP-LD105":"LD Coaching and Mentoring",
+        "DVP-LD108":"LD Annual and Ad-hoc Deployment",
+        "DVP-LD109":"LD PSLP Career Development Form",
+        "DVP-LD110":"LD AO Career Roadmap",
+        "DVP-LD111A":"LD Alpha Society Membership for Agency HR",
+        "DVP-LD111B":"LD Alpha Society Membership for LD AlphaSoc Admin, AlphaSoc Secretaries, Secretary, Treasurers",
+        "DVP-LD112":"LD Appointment"
     }
     return course[code]
 
